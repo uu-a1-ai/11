cd0d708b94cc089f0a747934cbc91f6075d2f460
uu-a1-ai <17737540680@163.com>
2025-12-24 22:04:26 +0800

    Add files via upload

---
+++ vite.config.cjs
@@ -0,0 +1,11 @@
+const { defineConfig } = require('vite')
+const vue = require('@vitejs/plugin-vue')
+
+module.exports = defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 3000,
+    open: true
+  }
+})
+
